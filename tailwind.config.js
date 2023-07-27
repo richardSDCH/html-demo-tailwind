9ae328d3f46a8b26428065ac936af5fd6e446aff
Ricardo Henriquez <ricardo@tradeledge.com>
2023-07-27 20:04:10 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home': "url(../src/images/Home-Default.png)",
+        'search': "url(../src/images/Search-Default.png)",
+        'network': "url(../src/images/Network-Default.png)",
+        'trade': "url(../src/images/Trade-Default.png)",
+        'manage': "url(../src/images/Manage-Default.png)",
+        'home-hover': "url(../src/images/Home-Hover.png)",
+        'search-hover': "url(../src/images/Search-Hover.png)",
+        'network-hover': "url(../src/images/Network-Hover.png)",
+        'trade-hover': "url(../src/images/Trade-Hover.png)",
+        'manage-hover': "url(../src/images/Manage-Hover.png)",
+      }
+    },
+  },
+  plugins: [],
+}
+
